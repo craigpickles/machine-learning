a9797f523ba9cbcd8e8e071c2e6aa8be69f1167a
Craig Pickles <craig.pickles@invently.co.uk>
2019-09-19 10:26:20 +0100

    Change to use ww2 temp data

--- linear-regression.py
+++ linear-regression.py
@@ -10,24 +10,30 @@ import numpy as np
 
 #%%
 # Import & setup data
-data = pd.read_csv('./data/housing-data.csv')
-x = data[['rm']]
-y = data[['medv']]
+
+data = pd.read_csv('./data/ww2-weather.csv') 
+x = data[['MinTemp']]
+y = data[['MaxTemp']]
+
+plt.scatter(x, y)
+plt.show()
 
 features = x.shape[1]
 
-x = (x - x.mean()) / x.std()
-xB = np.c_[np.ones((len(x),1)),x]
+xMean = x.mean()
+xStd = x.std()
+
+x = (x - xMean) / xStd
+x = np.c_[np.ones((len(x), 1)), x] # adding the intercept term
 
 iterations = 100
 learningRate = 0.1
-theta = np.random.randn(features+1, 1)
+theta = np.random.randn(features + 1, 1)
 
 #%%
 # Implementation
 
-# error between predicted values and expected values, only used to show a nice graph
-def calCost(theta, x, y):
+def calCost(theta, x, y): # error between predicted values and expected values, only used to show a nice graph
     m = y.size # number of training examples
     predictions = x.dot(theta)
     return (1 / (2 * m)) * np.sum(np.square(predictions - y))
@@ -41,7 +47,7 @@ def gradientDescent(x, y, theta, learningRate=0.01, iterations=100):
 
         prediction = np.dot(x,theta)
 
-        theta = theta - (learningRate * (1/m)) * x.T.dot((prediction - y))
+        theta = theta - (learningRate * (1 / m)) * x.T.dot((prediction - y))
         thetaHistory[it,:] = theta.T
         costHistory[it]  = calCost(theta, x, y)
 
@@ -53,30 +59,42 @@ def normalEquation(x, y):
 #%%
 # Plot data
 
-plt.figure(figsize=(10,6))
+plt.figure(figsize=(10, 6))
 
 plt1 = plt.subplot(2, 1, 1)
 plt1.set_title("Learning rate:{}".format(learningRate))
 
 plt2 = plt.subplot(2, 1, 2)
 plt2.set_title("Iterations:{}".format(iterations))
-plt2.plot(x, y, 'b.')
+plt2.plot(x[:,1], y, 'b.')
 
-theta = np.random.randn(features+1,1)
+theta = np.random.randn(features + 1, 1)
 
 costHistory = np.zeros(iterations)
 
 for i in range(iterations):
-    theta,h,_ = gradientDescent(xB, y, theta, learningRate, 1)
+    theta,h,_ = gradientDescent(x, y, theta, learningRate, 1)
     costHistory[i] = h[0]
     # plot data
-    plt2.plot(x, xB.dot(theta), 'r-')
+    plt2.plot(x[:,1], x.dot(theta), 'r-')
 
 # plot cost history
-plt1.plot(range(iterations),costHistory,'b.')
+plt1.plot(range(iterations), costHistory, 'b.')
 
 # plot the normal equation
-theta = normalEquation(xB, y)
-plt2.plot(x, xB.dot(theta), 'g-')
+theta = normalEquation(x, y)
+plt2.plot(x[:,1], x.dot(theta), 'g-')
 
 plt.show()
+
+#%%
+# Predict
+
+val = [30] # min temp
+val = (val - xMean) / xStd # normalize data using same mean and SD as training set
+val = np.c_[np.ones((len(val), 1)), val] 
+
+prediction = val.dot(theta)
+print('Prediction : ' + str(prediction[0][0]) + ' Max Temp')
+
+#%%
